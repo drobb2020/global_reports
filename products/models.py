3a909f38b215eaf250928a651ae8f676cab629a9
David Robb <66834033+drobb2020@users.noreply.github.com>
2022-04-22 20:20:17 -0400

    Removed migrations

--- products/models.py
+++ products/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=120)
     image = models.ImageField(upload_to='products', default='no_picture.png')
-    price = models.FloatField(help_text='in CAD dollars $')
+    price = models.DecimalField(help_text='in CAD dollars $')
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
